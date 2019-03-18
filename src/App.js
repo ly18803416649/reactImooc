fed08df35ef8c47d017bcbb976cb4fdb206835fd
rider <1187040770@qq.com>
2019-03-18 10:03:48 +0800

    环境

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'antd-mobile'
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <Button type="primary">按需加载</Button>
           <a
             className="App-link"
             href="https://reactjs.org"
